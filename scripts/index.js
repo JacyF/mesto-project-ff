40f3bf032c89caf777689b3e8431e4aa573bb326
FranciscoJ <brunoloouuiis@gmail.com>
2024-08-30 19:09:15 +0300

    added functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,57 @@
 // @todo: Темплейт карточки
 
+// Gettinng acess to template from HTML code
+const cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
+
+// Getting acess to node where to add the new card
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
+function createCard(card, cardDelete) {
+
+    // Cloning template to get acess to his content
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+    //Getting acess to the img node
+    const cardImage = cardElement.querySelector(".card__image");
+
+    //adding content to img node
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+
+    // Getting acess to the title node
+    const cardTitle = cardElement.querySelector(".card__title");
+
+    // adding content to title
+    cardTitle.textContent = card.name;
+
+    // Getting acess to the button delete card
+    const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+
+    // adding event to the button
+    cardDeleteButton.addEventListener("click", () => {
+
+        cardElement.remove();
+    });
+
+    return cardElement;
+}
+
 // @todo: Функция удаления карточки
 
+function cardDelete(card) {
+
+    card.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+// looping through cards to display them
+initialCards.forEach(card => {
+
+    // adding and displaying the cards
+    placesList.append(createCard(card));
+})
